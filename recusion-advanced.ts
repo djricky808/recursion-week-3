665ddf902df340a7b047624644c0be949f1f4330
Richard Olpindo <djricky088@yahoo.com>
2025-02-22 13:17:37 -1000

    Testing out strategy using a for loop.

--- recusion-advanced.ts
+++ recusion-advanced.ts
@@ -8,6 +8,37 @@
 // console.log(threeSum([0, 1, 1])); // Output: []
 // console.log(threeSum([0, 0, 0])); // Output: [[0, 0, 0]]
 
+/* Strategy:
+[1,2,3,4,5]
+In a set of 5 elements, the possible solutions are
+1,2,3 / 1,2,4 / 1,2,5 / 1,3,4/ 1,3,5/ 1,4,5
+2,3,4/ 2,3,5 / 2,4,5
+3,4,5/
+if we were to write this using for loops...
+loop through the third number in the trio for any possibilities
+Then repeat while iterating through the second number in the trio
+In move the i index up 1.
+*/
+
+function loopThreeSum(arr: number[]): number[][] | null[] {
+  let tripletSumToZero: number[][] = [];
+  for (let i = 0; i < arr.length - 2; i++) {
+    //omit the last two elements in the array since i would never be the 2nd or 3rd number in the triplet
+    for (let j = i + 1; j < arr.length - 1; j++) {
+      //j cannot be the last number
+      for (let k = j + 1; k < arr.length; k++) {
+        if (arr[i] + arr[j] + arr[k] === 0) {
+          tripletSumToZero.push([arr[i], arr[j], arr[k]]);
+        }
+      }
+    }
+  }
+  return tripletSumToZero;
+}
+
+console.log(loopThreeSum([-1, 0, 1, 2, -1, -4]));
+// Expected Output: [[-1, -1, 2], [-1, 0, 1]]
+
 function threeSum(arr) {
   // Your code here
 }
